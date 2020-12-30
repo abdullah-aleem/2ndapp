f156dcb55b2973cb5b9160bb1273f98a787aaf70
AbdullahAleem <abdullahaleem2102@gmail.com>
2020-12-30 21:27:47 +0500

    second commit

--- src/helpers/Counter.js
+++ src/helpers/Counter.js
@@ -3,10 +3,22 @@ import useGlobalCounter from './globalvar';
 
  export default function Counter(props) {
     const [state, setState] = useGlobalCounter()
-    useEffect(() => {setState(state + 1)}, []);
+//useEffect(() => {setState(state + 1)}, []);
     return (
       <div>
         <p>State: {state}</p>
       </div>
     )
 }
+
+// var card = new Card (){};
+
+// var cards = [ cardi1, card2, card3 .........];
+
+// var lot1 = Math.random(cards * 13);
+
+// for(id)
+// cards.removeat(lot1);
+
+
+// player1 = lot1;l
--- src/pages/playarea.js
+++ src/pages/playarea.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
 import { GlobalCounterProvider } from '../helpers/globalvar'
-import Counter from '../helpers/Counter' ;
+import Counter from '../helpers/Counter';
 
 class Playarea extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: null
+            error: null,
+            firstplayer:Array(12).fill(null),
+            Secondplayer:Array(12).fill(null),
+            thirdplayer:Array(12).fill(null),
+            forthplayer:Array(12).fill(null)
         };
-
+this.MakeCards=this.MakeCards.bind(this);
+this.Showfirstplayercards=this.Showfirstplayercards.bind(this);
+    }
+    MakeCards(){
+        const cards= Array(51).fill({null:null})
+        
+//    const cards={};
+   
+   for (let count=0;count<13;count++){
+           cards[count]={'Spades':count+2}
+           
+       }
+    for (let count=13;count<26;count++){
+        cards[count]={'Diamonds':count-11}
+        
+    }
+    for (let count=26;count<39;count++){
+        cards[count]={'Heart':count-24}
+        
+    }
+    for (let count=39;count<52;count++){
+        cards[count]={'Clubs':count-37}
+        
     }
     
+    
+    
+    const shuffled = cards.sort(() => 0.5 - Math.random());
+
+    this.setState({
+        firstplayer:shuffled.slice(0,13),
+        secondplayer:shuffled.slice(13,26),
+        thirdplayer:shuffled.slice(26,39),
+        fourthplayer:shuffled.slice(39,52)
+    })
+console.log(shuffled);
+    return;
+
+}
+
+
+Showfirstplayercards(){
+    this.setState({
+        firstplayer:this.state.firstplayer
+    })
+     console.log(this.state.firstplayer);
+}
+
     render() {
-        
+
         return (
             <div>
-            
-            
+
+
                 <div className='tailwindheader'>
                     <header class="text-gray-600 body-font">
                         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -27,38 +76,49 @@ class Playarea extends Component {
                                 <span class="ml-3 text-xl">Play Area Rang Game</span>
                             </a>
                             <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                                <a class="mr-5 hover:text-gray-900">Distribute</a>
-                                <a class="mr-5 hover:text-gray-900">declare winner</a>
-                                <a class="mr-5 hover:text-gray-900">Endgame</a>
+
+                                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" onClick={this.MakeCards}>Distribute
+                                    
+                                </button>
+
+
+                                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" onClick={this.Showfirstplayercards}>show player1
+                                    
+                                </button>
+
+
+                                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Endgame
+                                    
+                                </button>
                                 <a class="mr-5 hover:text-gray-900">Choose 1 player</a>
                             </nav>
-                            <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                </svg>
-                            </button>
+                            
                         </div>
                     </header>
                 </div>
 
                 <div className='Player1'>
                     <h1>player 1</h1>
+                    <h2></h2>
                 </div>
                 <div className='count'>
-                <GlobalCounterProvider>
-        <div style={{ margin: '20px' }}>
-          <Counter />
-        </div>
-      </GlobalCounterProvider>
+                    <GlobalCounterProvider>
+                        <div style={{ margin: '20px' }}>
+                            <Counter />
+                        </div>
+                    </GlobalCounterProvider>
                 </div>
                 <div className='Player2'>
                     <h1>player 2</h1>
+                    {/* <h2>{this.state.secondplayer}</h2> */}
                 </div>
                 <div className='Player3'>
                     <h1>player 3</h1>
+                    {/* <h2>{this.state.thirdplayer}</h2> */}
                 </div>
                 <div className='Player4'>
                     <h1>player 4</h1>
+                    {/* <h2>{this.state.fourthplayer.length}</h2> */}
                 </div>
             </div>
         );
